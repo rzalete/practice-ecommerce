4947b9720d27deccc92d67deedd8ca6c45506c3f
Your Name <you@example.com>
2026-03-20 00:24:20 +0800

    feat: add admin order page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Products from './pages/Products';
 import Cart from './pages/Cart';
 import Orders from './pages/Orders';
 import AdminProducts from './pages/AdminProducts';
+import AdminOrders from './pages/AdminOrders';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Products />} />
         <Route path="/admin/products" element={<AdminProducts />} />
+        <Route path="/admin/orders" element={<AdminOrders />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/cart" element={<Cart />} />
